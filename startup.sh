df2d02e30e168e06bb7ddd1650e6855e25f042c1
Etienne Colaitis <ecolaitis@gmail.com>
2016-10-28 22:04:28 +0200

    no need to reboot the wlan0 interface

--- startup.sh
+++ startup.sh
@@ -8,7 +8,7 @@ cp /root/eth-rig1/startup.sh /root/startup.sh
 
 chmod +x /root/startup.sh
 
-ifdown wlan0 && ifup -v wlan0
+# ifdown wlan0 && ifup -v wlan0
 route add default gw 192.168.0.1 wlan0
 
 bash /root/eth-rig1/mine.sh
